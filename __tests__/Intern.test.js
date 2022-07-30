0c14e979e4b4dc2d22902c38be80e074c316b23a
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-07-30 19:20:20 -0400

    Created Intern class with passing tests

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,37 @@
+const Intern = require("../lib/Intern");
+
+test("shows an Intern's school", () => {
+  const intern = new Intern(
+    "Bobby",
+    "Brown",
+    12,
+    "intern@intern.com",
+    "Carleton"
+  );
+
+  expect(intern.school).toEqual(expect.any(String));
+});
+
+test("gets an Intern's school", () => {
+  const intern = new Intern(
+    "Bobby",
+    "Brown",
+    12,
+    "intern@intern.com",
+    "Carleton"
+  );
+
+  expect(intern.getSchool()).toEqual(expect.objectContaining({}));
+});
+
+test("getRole() should be overridden to return 'Intern'", () => {
+  const intern = new Intern(
+    "Bobby",
+    "Brown",
+    12,
+    "intern@intern.com",
+    "Carleton"
+  );
+
+  expect(intern.getRole()).toMatchObject(new Intern());
+});
